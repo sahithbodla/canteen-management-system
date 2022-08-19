07b7e11db191c4f1f25d9061831041939a11f407
Sahith Bodla <sahith.bodla@gmail.com>
2022-08-19 20:23:19 +0530

    feat(admin): admin can sell items to an employee

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -5,15 +5,16 @@ import { Link } from 'react-router-dom';
 import { database } from '../../firebase';
 
 const Menu = (props) => {
-  const { setMenu, menu, currentUser, setListOfUsers } = props;
+  const { setMenu, menu, currentUser, setListOfUsers, listOfEmployees } = props;
 
   const [quantity, setQuantity] = useState(0);
   const [price, setPrice] = useState(0);
   const [name, setName] = useState('');
   const [show, setShow] = useState(false);
   const [iUid, setIUid] = useState('');
+  const [eUid, setEUid] = useState('');
   const [modal, setModal] = useState('');
-  const [buyingQuantity, setBuyingQuantity] = useState(1);
+  const [purchaseQuantity, setPurchaseQuantity] = useState(1);
   const admin = currentUser?.role === 'Admin';
   const employee = currentUser?.role === 'Employee';
 
@@ -21,16 +22,24 @@ const Menu = (props) => {
   const handleShow = (uid, modal) => {
     setShow(true);
     setModal(modal);
-    setIUid(uid);
-    setQuantity(menu?.[uid].quantity);
-    setPrice(menu?.[uid].price);
-    setName(menu?.[uid].name);
+    if (uid) {
+      setIUid(uid);
+      setQuantity(menu?.[uid].quantity);
+      setPrice(menu?.[uid].price);
+      setName(menu?.[uid].name);
+    }
   };
 
   useEffect(() => {
     setData('menu', setMenu);
     setData('users', setListOfUsers);
-  }, [setMenu, iUid, buyingQuantity]);
+  }, [setMenu, iUid, purchaseQuantity]);
+
+  useEffect(() => {
+    setQuantity(menu?.[iUid]?.quantity);
+    setPrice(menu?.[iUid]?.price);
+    setName(menu?.[iUid]?.name);
+  }, [iUid]);
 
   const updateItem = () => {
     const updatedItem = {
@@ -60,8 +69,8 @@ const Menu = (props) => {
   };
 
   const buyItem = () => {
-    const updatedQuantity = quantity - buyingQuantity;
-    const updatedWalletPrice = currentUser?.balance - price * buyingQuantity;
+    const updatedQuantity = quantity - purchaseQuantity;
+    const updatedWalletPrice = currentUser?.balance - price * purchaseQuantity;
     database
       .ref(`menu/${iUid}/quantity`)
       .set(updatedQuantity)
@@ -74,9 +83,34 @@ const Menu = (props) => {
       .catch((error) => {
         console.log(error.message);
       });
-    setBuyingQuantity(1);
+    setPurchaseQuantity(1);
     setIUid('');
     setShow(false);
+  };
+
+  const sellItem = () => {
+    const updatedQuantity = quantity - purchaseQuantity;
+    const updatedWalletPrice =
+      listOfEmployees?.[eUid]?.balance - price * purchaseQuantity;
+    database
+      .ref(`menu/${iUid}/quantity`)
+      .set(updatedQuantity)
+      .catch((error) => {
+        console.log(error.message);
+      });
+    database
+      .ref(`users/${eUid}/balance`)
+      .set(updatedWalletPrice)
+      .catch((error) => {
+        console.log(error.message);
+      });
+    setPurchaseQuantity(1);
+    setIUid('');
+    setEUid('');
+    setShow(false);
+    setName('');
+    setPrice('');
+    setQuantity('');
   };
 
   const editModalJSX = () => {
@@ -153,12 +187,12 @@ const Menu = (props) => {
             <Form.Label>Quantity {`(${quantity} left)`}</Form.Label>
             <Form.Control
               type="number"
-              value={buyingQuantity}
-              onChange={(val) => setBuyingQuantity(val.target.value)}
+              value={purchaseQuantity}
+              onChange={(val) => setPurchaseQuantity(val.target.value)}
               className="mb-2"
             ></Form.Control>
           </Form.Group>
-          {price * buyingQuantity} will be debited from your wallet
+          {price * purchaseQuantity} will be debited from your wallet
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
@@ -166,6 +200,69 @@ const Menu = (props) => {
           </Button>
           <Button variant="success" onClick={buyItem}>
             Buy {name}
+          </Button>
+        </Modal.Footer>
+      </>
+    );
+  };
+
+  const sellItemJSX = () => {
+    return (
+      <>
+        <Modal.Header>
+          <Modal.Title>Sell an Item ?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Group id="sellItem" className="mt-2">
+            <Form.Label>Select Item to Sell</Form.Label>
+            <Form.Control
+              as="select"
+              value={iUid}
+              onChange={(e) => setIUid(e.target.value)}
+              className="mb-2"
+            >
+              <option value="" selected>
+                Select
+              </option>
+              {Object.entries(menu).map(([uid, item]) => {
+                return <option value={uid}>{item.name}</option>;
+              })}
+            </Form.Control>
+          </Form.Group>
+          <Form.Group id="sellEmployee" className="mt-2">
+            <Form.Label>Select Employee who is buying</Form.Label>
+            <Form.Control
+              as="select"
+              value={eUid}
+              onChange={(e) => setEUid(e.target.value)}
+              className="mb-2"
+            >
+              <option value="" selected>
+                Select
+              </option>
+              {Object.values(listOfEmployees).map((employee) => {
+                return <option value={employee.uid}>{employee.name}</option>;
+              })}
+            </Form.Control>
+          </Form.Group>
+          <Form.Group id="sellQuantity" className="mt-2">
+            <Form.Label>Quantity {`(${quantity} left)`}</Form.Label>
+            <Form.Control
+              type="number"
+              value={purchaseQuantity}
+              onChange={(val) => setPurchaseQuantity(val.target.value)}
+              className="mb-2"
+            ></Form.Control>
+          </Form.Group>
+          {price * purchaseQuantity} will be debited from{' '}
+          {listOfEmployees[eUid]?.name}'s wallet
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button variant="success" onClick={sellItem}>
+            Sell {name}
           </Button>
         </Modal.Footer>
       </>
@@ -219,15 +316,23 @@ const Menu = (props) => {
             </tbody>
           </table>
           {admin && (
-            <Button className="w-100 mt-4">
-              <Link
-                to="/add-item"
-                role="button"
-                className="text-decoration-none text-white"
+            <div className="d-flex">
+              <Button className="w-100 mx-2">
+                <Link
+                  to="/add-item"
+                  role="button"
+                  className="text-decoration-none text-white"
+                >
+                  Add an Item
+                </Link>
+              </Button>
+              <Button
+                className="w-100 mx-2"
+                onClick={() => handleShow(iUid, 'SELL')}
               >
-                Add an Item
-              </Link>
-            </Button>
+                Sell an Item
+              </Button>
+            </div>
           )}
         </Card.Body>
       </Card>
@@ -235,6 +340,7 @@ const Menu = (props) => {
         {modal === 'EDIT' && editModalJSX()}
         {modal === 'DELETE' && deleteModalJSX()}
         {modal === 'BUY' && buyItemJSX()}
+        {modal === 'SELL' && sellItemJSX()}
       </Modal>
     </div>
   );
